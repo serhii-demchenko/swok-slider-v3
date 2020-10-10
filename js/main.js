83e7dd6bb0e5e838df5c497c9c58ef2912e0fb12
Serhii Demchenko HPC <dem.s.vl@gmail.com>
2020-10-10 21:52:05 +0300

    fixed formating

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-import {default as SwokSlider} from './swokSlider.js';
+import { default as SwokSlider } from './swokSlider.js';
 
 const mySlider = new SwokSlider({
-  transitionDuration: "500ms",
-  delay: "5000ms",
+  transitionDuration: '500ms',
+  delay: '5000ms',
 });
 const mySlider2 = new SwokSlider({
-  transitionDuration: "250ms",
-  sliderClass: ".swok-slider2",
-  delay: '2000ms'
+  transitionDuration: '250ms',
+  sliderClass: '.swok-slider2',
+  delay: '2000ms',
 });
--- js/swokSlider.js
+++ js/swokSlider.js
@@ -1,93 +1,108 @@
 export default class {
-	constructor({
-		sliderClass = ".swok-slider",
-		transitionDuration = "250ms",
-		backgroundColor = "rgba(0, 0, 0, 0.5)",
-		delay = "4000",
-	}) {
-		this.slider = document.querySelector(sliderClass);
-		this.transitionDuration = transitionDuration;
-		this.slider.style.overflow = "hidden";
-		this.slider.style.backgroundColor = backgroundColor;
-		this.slider.style.position = "relative";
-		this.sliderCount = this.slider.childElementCount;
-		this.curentSlide = 0;
-		this.delay = Number.parseInt(delay);
-		this.prevButton = this.createSliderElement("button", "prev");
-		this.nextButton = this.createSliderElement("button", "next");
-		this.appendElements(this.wrapSlider(), this.prevButton, this.nextButton);
-		this.avtoFlipping = setInterval(this.nextSlideHandler.bind(this), this.delay);
-		
-		this.resizeHandler();
+  constructor({
+    sliderClass = '.swok-slider',
+    transitionDuration = '250ms',
+    backgroundColor = 'rgba(0, 0, 0, 0.5)',
+    delay = '4000',
+  }) {
+    this.slider = document.querySelector(sliderClass);
+    this.transitionDuration = transitionDuration;
+    this.slider.style.overflow = 'hidden';
+    this.slider.style.backgroundColor = backgroundColor;
+    this.slider.style.position = 'relative';
+    this.sliderCount = this.slider.childElementCount;
+    this.curentSlide = 0;
+    this.delay = Number.parseInt(delay);
+    this.prevButton = this.createSliderElement('button', 'prev');
+    this.nextButton = this.createSliderElement('button', 'next');
+    this.appendElements(this.wrapSlider(), this.prevButton, this.nextButton);
+    this.avtoFlipping = setInterval(
+      this.nextSlideHandler.bind(this),
+      this.delay,
+    );
 
-		window.addEventListener("resize", this.resizeHandler.bind(this));
-		this.slider.addEventListener("click", this.clickHandler.bind(this));
-	}
-	wrapSlider() {
-		const wrapper = document.createElement("div");
-		wrapper.classList.add("swok-slider__wrapper");
-		for (let i = 0; i < this.sliderCount; i += 1) {
-			wrapper.append(this.slider.children[i].cloneNode(true));
-			wrapper.children[i].classList.add("swok-slider__item");
-			wrapper.children[i].style.width = `calc(100% / ${this.sliderCount})`;
-		}
-		for (let i = 0; i < this.sliderCount; i += 1) {
-			this.slider.removeChild(this.slider.children[0]);
-		}
-		return wrapper;
-	}
-	appendElements(...args) {
-		this.slider.append(...args);
-	}
-	createSliderElement(element, modifier) {
-		const elmt = document.createElement(element);
-		elmt.classList.add("swok-slider__" + element);
-		if (modifier) {
-			elmt.classList.add("swok-slider__" + element + "-" + modifier);
-		}
-		return elmt;
-	}
-	setSlidersWidth() {
-		for (let i = 0; i < this.slider.children.length; i += 1) {
-			if (this.slider.children[i].classList.contains("swok-slider__wrapper")) {
-				this.sliderWidth = this.slider.children[i].style.width = this.slider.clientWidth * this.sliderCount + "px";
-				break;
-			}
-		}
-	}
-	calculateSlideCoordinates() {
-		this.sliderCoordinateArray = [];
-		for (let i = 0; i < this.sliderCount; i += 1) {
-			this.sliderCoordinateArray.push(0 - (Number.parseInt(this.sliderWidth) / this.sliderCount) * i);
-		}
-	}
-	changeSlide(enableAnimation) {
-		this.slider.children[0].style.transform = `translate3D(${this.sliderCoordinateArray[this.curentSlide]}px, 0, 0)`;
-		this.slider.children[0].style.transition = enableAnimation ? `transform ${this.transitionDuration}` : "none";
-	}
-	prevSlideHandler() {
-		this.curentSlide = this.curentSlide - 1 > 0 ? this.curentSlide - 1 : this.sliderCount - 1;
-		this.changeSlide(true);
-	}
-	nextSlideHandler() {
-		this.curentSlide += this.curentSlide + 1 === this.sliderCount ? -this.sliderCount + 1 : 1;
-		this.changeSlide(true);
-	}
-	resizeHandler() {
-		this.setSlidersWidth();
-		this.calculateSlideCoordinates();
-		this.changeSlide();
-	}
-	clickHandler(e) {
-		if (e.target === this.nextButton) {
-			this.suspenseAutoFill(this.nextSlideHandler.bind(this));
-		} else if (e.target === this.prevButton) {
-			this.suspenseAutoFill(this.prevSlideHandler.bind(this))
-		}
-	}
-	suspenseAutoFill(cb) {
-		clearInterval(this.avtoFlipping);
-		cb();
-		this.avtoFlipping = setInterval(this.nextSlideHandler.bind(this), this.delay);
-	}
+    this.resizeHandler();
+
+    window.addEventListener('resize', this.resizeHandler.bind(this));
+    this.slider.addEventListener('click', this.clickHandler.bind(this));
+  }
+  wrapSlider() {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('swok-slider__wrapper');
+    for (let i = 0; i < this.sliderCount; i += 1) {
+      wrapper.append(this.slider.children[i].cloneNode(true));
+      wrapper.children[i].classList.add('swok-slider__item');
+      wrapper.children[i].style.width = `calc(100% / ${this.sliderCount})`;
+    }
+    for (let i = 0; i < this.sliderCount; i += 1) {
+      this.slider.removeChild(this.slider.children[0]);
+    }
+    return wrapper;
+  }
+  appendElements(...args) {
+    this.slider.append(...args);
+  }
+  createSliderElement(element, modifier) {
+    const elmt = document.createElement(element);
+    elmt.classList.add('swok-slider__' + element);
+    if (modifier) {
+      elmt.classList.add('swok-slider__' + element + '-' + modifier);
+    }
+    return elmt;
+  }
+  setSlidersWidth() {
+    for (let i = 0; i < this.slider.children.length; i += 1) {
+      if (this.slider.children[i].classList.contains('swok-slider__wrapper')) {
+        this.sliderWidth = this.slider.children[i].style.width =
+          this.slider.clientWidth * this.sliderCount + 'px';
+        break;
+      }
+    }
+  }
+  calculateSlideCoordinates() {
+    this.sliderCoordinateArray = [];
+    for (let i = 0; i < this.sliderCount; i += 1) {
+      this.sliderCoordinateArray.push(
+        0 - (Number.parseInt(this.sliderWidth) / this.sliderCount) * i,
+      );
+    }
+  }
+  changeSlide(enableAnimation) {
+    this.slider.children[0].style.transform = `translate3D(${
+      this.sliderCoordinateArray[this.curentSlide]
+    }px, 0, 0)`;
+    this.slider.children[0].style.transition = enableAnimation
+      ? `transform ${this.transitionDuration}`
+      : 'none';
+  }
+  prevSlideHandler() {
+    this.curentSlide =
+      this.curentSlide - 1 > 0 ? this.curentSlide - 1 : this.sliderCount - 1;
+    this.changeSlide(true);
+  }
+  nextSlideHandler() {
+    this.curentSlide +=
+      this.curentSlide + 1 === this.sliderCount ? -this.sliderCount + 1 : 1;
+    this.changeSlide(true);
+  }
+  resizeHandler() {
+    this.setSlidersWidth();
+    this.calculateSlideCoordinates();
+    this.changeSlide();
+  }
+  clickHandler(e) {
+    if (e.target === this.nextButton) {
+      this.suspenseAutoFill(this.nextSlideHandler.bind(this));
+    } else if (e.target === this.prevButton) {
+      this.suspenseAutoFill(this.prevSlideHandler.bind(this));
+    }
+  }
+  suspenseAutoFill(cb) {
+    clearInterval(this.avtoFlipping);
+    cb();
+    this.avtoFlipping = setInterval(
+      this.nextSlideHandler.bind(this),
+      this.delay,
+    );
+  }
 }
